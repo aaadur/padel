9f750353e54c3295256393b28800387dd72331b8
aaadur <56845103+aaadur@users.noreply.github.com>
2023-03-30 10:39:55 +0200

    Update robot.py

--- robot.py
+++ robot.py
@@ -12,6 +12,7 @@ from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
 from selenium.webdriver.firefox.service import Service
+from selenium.webdriver.firefox.options import Options
 from xml.dom.minidom import Element, parse
 import xml.dom.minidom
 import sys
